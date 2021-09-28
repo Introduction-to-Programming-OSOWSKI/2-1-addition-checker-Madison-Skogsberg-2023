82768a5bcfd2dc4284b8354080580d344a951bba
Madison-Skogsberg-2023 <816418@mystma.org>
2021-09-28 08:59:31 -0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 #WRITE YOUR CODE HERE
+#define function
+#add parameters
+def addCheck(x, y, z):
+#if then else
+    if x + y == z:
+        return "True"
+    else:
+        return "False"
+#run function
+print(addCheck(10,2,22))
